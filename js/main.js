22fdade5e6d7c92a7581c9aaa3a37e7f90b274a9
kiranlal2 <kiranlal6dx@gmail.com>
2025-07-17 14:37:50 +0530

    dashboard first code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
 // components linking
 
-function loadComponents(selecter, url) {
+function loadComponents(selector, url) {
     fetch(url)
         .then(response => response.text())
-        .then(html => document.querySelectorAll(selecter).innerHTML = html)
+        .then(html => document.querySelector(selector).innerHTML = html)
         .catch(error => console.error('Error loading components:', error));
 }
 loadComponents('.sidebar', './components/sidebar.html');
 loadComponents('.header', './components/header.html');
 loadComponents('.card', './components/card.html');
+
+// profile dropdown
+function profileDropdown() {
+    const dropdown = document.querySelector('.dropdown-menu');
+    dropdown.classList.toggle('show');
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+        const dropdowns = document.querySelectorAll('.dropdown-menu');
+        dropdowns.forEach(dropdown => {
+            if (dropdown.classList.contains('show')) {
+                dropdown.classList.remove('show');
+            }
+        });
+    }
+}
